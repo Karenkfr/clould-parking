9213b8beb38988e22447d83c7f6e3bca7b5054cd
Ribeiro <karen.ribeiro@capgemini.com>
2023-07-13 09:51:36 -0300

    build application completed v1

--- src/main/java/br/com/karen/Parking/controller/ParkingController.java
+++ src/main/java/br/com/karen/Parking/controller/ParkingController.java
@@ -1,16 +1,16 @@
-package controller;
+package br.com.karen.Parking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class HelloController {
+@RequestMapping("/")
+public class ParkingController {
 	
 	@GetMapping
 	public String hello() {
-		return "hello word";
+		return "hello word!";
 	}
 
 }
